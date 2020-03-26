b7308aec62f5e799df289f83e19fbc0948338dc5
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-03-26 12:50:41 -0300

    Create route to insert and list about the ONG

---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const ongController = require('./controller/ong-controller')
+
+const routes = express.Router()
+
+routes.get('/ongs', ongController.index)
+routes.post('/ongs', ongController.create)
+
+module.exports = routes
