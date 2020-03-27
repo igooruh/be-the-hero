17190ea3145abef0904fe7f42031899c3679829b
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-03-27 01:47:25 -0300

    Create routes session, profile and incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,25 @@
 const express = require('express')
 
 const ongController = require('./controller/ong.controller')
+const incidentController = require('./controller/incident.controller')
+const profileController = require('./controller/profile-incident.controller')
+const sessionController = require('./controller/session.controller')
 
 const routes = express.Router()
 
+// Session
+routes.post('/session', sessionController.initSession)
+
+// ONG
 routes.get('/ongs', ongController.index)
 routes.post('/ongs', ongController.create)
+
+// Incident
+routes.post('/incidents', incidentController.insertIncident)
+routes.get('/incidents', incidentController.listIncidentsAll)
+routes.delete('/incidents/:id', incidentController.deleteIncident)
+
+// Profile
+routes.get('/profile', profileController.incidentOne)
 
 module.exports = routes
