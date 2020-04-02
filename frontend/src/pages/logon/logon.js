4f54a148814ded0ff15e96068f07aaba766d04f1
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-04-02 02:04:16 -0300

    Create login, validation on database

--- frontend/src/pages/logon/logon.js
+++ frontend/src/pages/logon/logon.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './logon-style.scss';
 import LogoBeTheHero from '../../components/logoBeTheHero/logo-be-the-hero';
 import heroesImg from '../../assets/heroes.png';
+import api from '../../services/api';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    const handleLogin = async e => {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('session', { id });
+
+            // Deixando identifacação na sessão
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch(err) {
+            alert('Falha no login, tente novamente');
+            console.error(err);
+        }
+    }
+
     return(
         <div className="logon-container">
             <section className="form">
                 <LogoBeTheHero />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu Logon</h1>
 
-                    <input placeholder="Sua ID"/>
+                    <input placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)} />
                     <button type="submit" className="button-general">Login</button>
 
                     <Link className="links-app" to="/register">
